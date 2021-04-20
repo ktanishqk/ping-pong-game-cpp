e6638bb10cd09e5d28f4ceff865a50f00176bbd0
ktanishqk <68249492+ktanishqk@users.noreply.github.com>
2021-04-20 02:39:55 -0500

    Created Paddle header and source file

---
+++ include/paddle.h
@@ -0,0 +1,18 @@
+//
+// Created by Tanishq Khurana on 4/20/21.
+//
+#include "cinder/gl/gl.h"
+#pragma once
+namespace pingpong {
+    class Paddle {
+    private:
+        int length_;
+        int width_;
+        ci::Color color_;
+    };
+}
+
+#ifndef FINAL_PROJECT_KTANISHQK_PADDLE_H
+#define FINAL_PROJECT_KTANISHQK_PADDLE_H
+
+#endif //FINAL_PROJECT_KTANISHQK_PADDLE_H
---
+++ src/paddle.cc
@@ -0,0 +1,7 @@
+//
+// Created by Tanishq Khurana on 4/20/21.
+//
+
+#include "paddle.h"
+
+
