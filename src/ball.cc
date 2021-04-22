85c5153ed1f04a5e230b58588c21d3889dd5e180
ktanishqk <68249492+ktanishqk@users.noreply.github.com>
2021-04-22 00:07:46 -0500

    Ball class methods and variables created, CMakeLists.txt updated

--- src/ball.cc
+++ src/ball.cc
@@ -2,3 +2,42 @@
 // Created by Tanishq Khurana on 4/20/21.
 //
 #include "ball.h"
+namespace pingpong {
+Ball::Ball(int radius, ci::Color color, glm::vec2 position, glm::vec2 velocity) {
+  this->radius_ = radius;
+  this->color_ = color;
+  this->velocity_ = velocity;
+  this->position_ = position;
+}
+glm::vec2 Ball::GetPosition() {
+  return position_;
+}
+
+glm::vec2 Ball::GetVelocity() {
+  return velocity_;
+}
+
+int Ball::GetRadius() {
+  return radius_;
+}
+
+ci::Color Ball::GetColor() {
+  return color_;
+}
+
+void Ball::SetPosition(const glm::vec2& position) {
+  this->position_ = position;
+}
+
+void Ball::SetVelocity(const glm::vec2& velocity) {
+  this->velocity_ = velocity;
+}
+
+void Ball::SetColor(const ci::Color& color) {
+  this->color_ = color;
+}
+
+void Ball::SetRadius(int radius) {
+  this->radius_ = radius;
+}
+}
