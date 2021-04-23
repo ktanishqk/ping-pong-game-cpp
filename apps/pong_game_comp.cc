7660257569cd0d22fb20c213af6403a81356863b
ktanishqk <68249492+ktanishqk@users.noreply.github.com>
2021-04-23 13:35:54 -0500

    Minor bugs fixed

--- apps/pong_game_comp.cc
+++ apps/pong_game_comp.cc
@@ -2,3 +2,17 @@
 // Created by Tanishq Khurana on 4/20/21.
 //
 
+#include <pong_simulation.h>
+#include <pong_table.h>
+
+using pingpong::PongSimulation;
+
+void prepareSettings(PongSimulation::Settings* settings) {
+  settings->setResizable(false);
+}
+
+// This line is a macro that expands into an "int main()" function.
+CINDER_APP(PongSimulation, ci::app::RendererGl, prepareSettings);
+
+
+
