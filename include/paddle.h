05ba950bc605a5772f5540a83fde067138bfdf0d
ktanishqk <68249492+ktanishqk@users.noreply.github.com>
2021-04-23 13:35:07 -0500

    Bugs removed and default constructor added

--- include/paddle.h
+++ include/paddle.h
@@ -2,13 +2,21 @@
 // Created by Tanishq Khurana on 4/20/21.
 //
 #include "cinder/gl/gl.h"
+#include <cinder/Color.h>
+
 #pragma once
 namespace pingpong {
     class Paddle {
     private:
-        int length_;
-        int width_;
-        ci::Color color_;
+        glm::vec2 top_left_position_;
+        glm::vec2 bottom_right_position_;
+    public:
+     Paddle();
+     Paddle(glm::vec2 top_right_position, glm::vec2 bottom_left_position);
+     void SetTopLeftPosition(const glm::vec2& position);
+     const glm::vec2& GetTopLeftPosition() const;
+      void SetBottomRightPosition(const glm::vec2& position);
+      const glm::vec2& GetBottomRightPosition() const;
     };
 }
 
--- src/paddle.cc
+++ src/paddle.cc
@@ -3,5 +3,25 @@
 //
 
 #include "paddle.h"
+namespace pingpong {
+Paddle::Paddle() {
+}
+Paddle::Paddle(glm::vec2 top_left_position, glm::vec2 bottom_right_position) {
 
+  this->top_left_position_ = top_left_position;
+  this->bottom_right_position_ = bottom_right_position;
+}
+const glm::vec2& Paddle::GetTopLeftPosition() const {
+  return top_left_position_;
+}
+void Paddle::SetTopLeftPosition(const glm::vec2& position) {
+  top_left_position_ = position;
+}
+const glm::vec2& Paddle::GetBottomRightPosition() const {
+  return bottom_right_position_;
+}
+void Paddle::SetBottomRightPosition(const glm::vec2& position) {
+  bottom_right_position_ = position;
+}
+}
 
