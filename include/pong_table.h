51be175e4e2b365d1a687cbbddf8f413ca8eefb0
ktanishqk <68249492+ktanishqk@users.noreply.github.com>
2021-04-23 13:35:41 -0500

    Created two paddles on the screen and a pong table

--- include/pong_table.h
+++ include/pong_table.h
@@ -7,19 +7,21 @@
 
 #endif //FINAL_PROJECT_KTANISHQK_PONG_TABLE_H
 #pragma once
+
 #include <cinder/Color.h>
+#include <paddle.h>
+#include <ball.h>
+#include "../../../include/cinder/app/App.h"
+#include "../../../include/glm/vec2.hpp"
 using glm::vec2;
 
-namespace pingpong{
+namespace pingpong {
     class PongTable {
     public:
         /**
          * Constructor for a ball
          */
-        PongTable(int kTopEdge,
-        int kBottomEdge,
-        int kLeftEdge,
-        int kRightEdge);
+        PongTable();
         /**
          * Displays the pong table
          */
@@ -29,10 +31,22 @@ namespace pingpong{
          */
         void AdvanceOneFrame();
 
-private:
-    int kTopEdge;
-    int kBottomEdge;
-    int kLeftEdge;
-    int kRightEdge;
+
+    private:
+      Paddle paddle1;
+      Paddle paddle2;
+      // Size of the window
+      const std::size_t kWindowSize = 900;
+      const std::size_t kLeftEdge = 100;
+      // This is the right edge coordinate
+      const std::size_t kRightEdge = 800;
+//      // This is the right boundary coordinate
+//      const std::size_t kRightBoundary = 500;
+//      // This is the bottom boundary coordinate
+//      const std::size_t kBottomBoundary = 300;
+      // This is the top edge coordinate
+      const std::size_t kTopEdge = 100;
+      // This is the bottom edge coordinate
+    const std::size_t kBottomEdge = 800;
     };
 }
--- src/pong_table.cc
+++ src/pong_table.cc
@@ -3,19 +3,22 @@
 //
 #include "pong_table.h"
 
-#include <paddle.h>
 
 namespace pingpong {
-PongTable::PongTable(int kTopEdge, int kBottomEdge, int kLeftEdge,
-                     int kRightEdge) {
-  this->kTopEdge = kTopEdge;
-  this->kLeftEdge = kLeftEdge;
-  this->kBottomEdge = kBottomEdge;
-  this->kRightEdge = kRightEdge;
+PongTable::PongTable() {
+  ci::app::setWindowSize(kWindowSize, kWindowSize);
+  paddle1 = Paddle(vec2(500, 600), vec2(400, 650));
+  paddle2 = Paddle(vec2(500, 200), vec2(400, 250));
 }
 void PongTable::Display() const {
-  ci::gl::drawStrokedRect(
-      ci::Rectf(vec2(kLeftEdge, kTopEdge),
-                     vec2(kRightEdge, kBottomEdge)));
+  ci::gl::color(ci::Color("red"));
+  ci::gl::drawSolidRect(
+      ci::Rectf(vec2(kLeftEdge, kTopEdge), vec2(kRightEdge, kBottomEdge)));
+  ci::gl::color(ci::Color("blue"));
+  ci::gl::drawSolidRect(ci::Rectf(paddle1.GetTopLeftPosition(), paddle1.GetBottomRightPosition()));
+  ci::gl::color(ci::Color("green"));
+  ci::gl::drawSolidRect(ci::Rectf(paddle2.GetTopLeftPosition(), paddle2.GetBottomRightPosition()));
+}
+void PongTable::AdvanceOneFrame() {
 }
 }  // namespace pingpong
