7d48ac9486f10d5f57a4358d66fb0b13aeb5ec6d
ktanishqk <68249492+ktanishqk@users.noreply.github.com>
2021-05-04 12:28:27 -0500

    Created support for the color of the ball

--- include/ball.h
+++ include/ball.h
@@ -5,8 +5,8 @@
 #ifndef FINAL_PROJECT_KTANISHQK_BALL_H
 #define FINAL_PROJECT_KTANISHQK_BALL_H
 #pragma once
-#include <cinder/Color.h>
 #include "cinder/gl/gl.h"
+#include <cinder/Color.h>
 using glm::vec2;
 namespace pingpong {
 class Ball {
@@ -14,6 +14,7 @@ class Ball {
   int radius_;
   glm::vec2 velocity_;
   glm::vec2 position_;
+  ci::Color color_;
 
  public:
   /**
@@ -23,7 +24,7 @@ class Ball {
   /**
    * Constructor for a ball that sets the radius, position, and the velocity of the ball
    */
-  Ball(int radius, glm::vec2 position, glm::vec2 velocity);
+  Ball(int radius, glm::vec2 position, glm::vec2 velocity, ci::Color color);
   /**
    * Display function for the ball
    */
@@ -38,8 +39,8 @@ class Ball {
    * Simple getter of the velocity of the ball
    * @return the velocity vector of the ball
    */
-  glm::vec2& GetVelocity();
-/**
+  glm::vec2 &GetVelocity();
+  /**
  * Simple getter for the radius of the ball
  * @return integer representing the radius of the ball
  */
@@ -48,6 +49,10 @@ class Ball {
    * Change the position of the ball to make the ball move
    */
   void ChangeBallPosition();
+  /**
+   * Simple Getter for the color of the ball
+   */
+   ci::Color &GetColor();
 };
-}  // namespace pingpong
-#endif  // FINAL_PROJECT_KTANISHQK_BALL_H
+}// namespace pingpong
+#endif// FINAL_PROJECT_KTANISHQK_BALL_H
